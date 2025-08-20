1617886d962357ea519ccf1c4c3bf7ac81655dea
betterlmy <betterlmy@icloud.com>
2025-08-21 01:21:20 +0800

    feat:init

---
+++ LmyTodoList/LmyTodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  LmyTodoListApp.swift
+//  LmyTodoList
+//
+//  Created by Zane on 2025/8/21.
+//
+
+import SwiftUI
+
+@main
+struct LmyTodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
